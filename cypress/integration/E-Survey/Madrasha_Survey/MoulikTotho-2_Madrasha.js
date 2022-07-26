c793984ba9304215dd8a310d2aee812126be8fa6
Selim <selimahmed1305@gmail.com>
2022-07-26 17:15:44 +0600

    codebase updated

---
+++ cypress/integration/E-Survey/Madrasha_Survey/MoulikTotho-2_Madrasha.js
@@ -0,0 +1,19 @@
+export class MoulikTotho_2{
+
+    madrashaSurvey_moulik_totho_2(){
+    //Section 1.5
+        //1.5.1 Land related information
+    // cy.xpath("")
+    // cy.xpath("")
+    // cy.xpath("")
+    // cy.xpath("")
+    // cy.xpath("")    
+
+    // section 1.5.2
+    cy.xpath("//input[@name='onePointFive.inPremiseTotalLand']")
+    cy.xpath("//input[@name='onePointFive.outPremiseTotalLand']")
+
+
+
+    }
+}
